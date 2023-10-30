faf5d904ee2494cb86781b8beb7a802388fa72c7
tanvircancode <tanahmchw241996@gmail.com>
2023-10-30 23:40:47 +0600

    starting TypeScript

---
+++ ts-class/sum.js
@@ -0,0 +1,5 @@
+"use strict";
+function calculateSum(a, b) {
+    return a + b;
+}
+console.log(calculateSum(15, 3));
---
+++ ts-class/sum.ts
@@ -0,0 +1,5 @@
+function calculateSum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(calculateSum(15, 3));
